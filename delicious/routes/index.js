6ad71db547b1432abe8608ff5bf7775027aa682d
Attila Vago <attilavago85@gmail.com>
2019-04-01 20:51:13 +0100

    lesson 6 done

--- delicious/routes/index.js
+++ delicious/routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  res.render('hello', {
+    name: 'attila',
+    dog: req.query.dog,
+    title: 'I love food',
+  });
 });
 
 module.exports = router;
