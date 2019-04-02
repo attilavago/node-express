5882548cb2d5becdb97fa0254aff34c458acc820
Attila Vago <attilavago85@gmail.com>
2019-04-02 23:12:08 +0100

    lesson 9 done

--- delicious/controllers/storeController.js
+++ delicious/controllers/storeController.js
@@ -1,9 +1,3 @@
-exports.myMiddleWare = (req, res, next) => {
-  req.name = 'attila';
-  res.cookie('name', 'attila is cool', {maxAge: 9000000});
-  next();
-}
-
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
